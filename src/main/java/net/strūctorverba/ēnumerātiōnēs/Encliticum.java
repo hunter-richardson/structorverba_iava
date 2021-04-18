1d95c5da0e08e2d2cd558ee542209be388b4b4e0
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2021-04-17 22:25:06 -0500

    Continue Javadoc additions

--- src/main/java/net/strūctorverba/ēnumerātiōnēs/Encliticum.java
+++ src/main/java/net/strūctorverba/ēnumerātiōnēs/Encliticum.java
@@ -3,15 +3,42 @@ package net.strūctorverba.ēnumerātiōnēs;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Ēnumerātiō haec <a href="https://en.wiktionary.org/wiki/Category:Latin_clitics">encliticum</a> subiūnctum reī omnī classis {@link VerbumMultiplex} dēsignat.
+ * @see VerbumMultiplex
+ */
 @SuppressWarnings({ "NonAsciiCharacters", "SpellCheckingInspection", "unused" })
 public enum Encliticum {
+  /**
+   * Valor hic <a href="https://en.wiktionary.org/wiki/-ne#Latin">encliticum interrogāns</a> dēsignat.
+   */
   INTERROGĀNS("ne"),
+
+  /**
+   * Valor hic <a href="https://en.wiktionary.org/wiki/-que#Latin">encliticum coniugāns</a> dēsignat.
+   */
   CONIUGĀNS("que"),
-  ĒLIĒNS("ve"),
+
+  /**
+   * Valor hic <a href="https://en.wiktionary.org/wiki/-que#Latin">encliticum ēligēns</a> dēsignat.
+   */
+  ĒLIGĒNS("ve"),
+
+  /**
+   * Valor hic encliticum nūllum dēsignat.
+   */
   NŌLĒNS(StringUtils.EMPTY);
 
+
+  /**
+   * Valor hic repraesentātiōnem scrīpta reī huius dēsignat.
+   */
   public final String scrīptiō;
 
+  /**
+   * Officium hoc cōnstrūctōrem reī classis huius perpetrat.
+   * @param scrpt valōrem {@link #scrīptiō} indicat.
+   */
   Encliticum(@NotNull final String scrpt) {
     scrīptiō = scrpt;
   }
