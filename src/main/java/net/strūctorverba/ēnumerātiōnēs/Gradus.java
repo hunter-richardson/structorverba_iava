282495b5059e1edd178be538683656cdff317418
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2021-04-17 22:33:01 -0500

    Continue Javadoc additions

--- src/main/java/net/strūctorverba/ēnumerātiōnēs/Gradus.java
+++ src/main/java/net/strūctorverba/ēnumerātiōnēs/Gradus.java
@@ -7,32 +7,75 @@ import org.jetbrains.annotations.*;
 
 import java.util.stream.Stream;
 
+/**
+ * Ēnumerātiō haec potentiam adiectīvī omnis dēsignat.
+ */
 @SuppressWarnings({ "NonAsciiCharacters", "SpellCheckingInspection", "unused" })
 public enum Gradus {
+  /**
+   * Valor hic ūtātur sī ūsūfructuāriī valōrem validum nōn dēnotat.
+   * Numquam scrīnium <a href="../src/main/resources">auxiliārēs</a> inscrībētur.
+   */
   NŪLLUM(StringUtils.EMPTY),
+
+  /**
+   * Valor hic potentiam nōrmāle dēsignat.
+   */
   NŌRMĀlE("nōrmāle"),
+
+  /**
+   * Valor hic potentiam comparātīvum dēsignat.
+   */
   COMPARĀTĪVUM("comparātīvum"),
+
+  /**
+   * Valor hic potentiam superlātīvum dēsignat.
+   */
   SUPERLĀTĪVUM("superlātīvum");
 
   @Getter(lazy = true)
   private static final @NotNull Nūntius.NūntiusErrōribus nūntius = Nūntius.NūntiusErrōribus.fac.get();
 
+  /**
+   * Valor hic repraesentātiōnem scrīpta reī huius dēsignat.
+   */
   public final @NotNull String scrīptiō;
 
+  /**
+   * Officium hoc cōnstrūctōrem reī classis huius perpetrat.
+   * @param scrpt valōrem {@link #scrīptiō} indicat.
+   */
   Gradus(@NotNull final String scrpt) {
     scrīptiō = scrpt;
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Gradus} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Gradus} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @implSpec {@code null} veniet sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Gradus dēfīniam(@NotNull final String scrīptiō) {
     return dēfīniam(scrīptiō, null);
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Gradus} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Gradus} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @param dēfecta valor reventus sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Gradus dēfīniam(@NotNull final String scrīptiō, @Nullable final Gradus dēfecta) {
     return Stream.of(values())
                  .filter(gradus -> gradus.scrīptiō.equals(scrīptiō))
                  .findFirst().orElse(dēfecta);
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Gradus} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @param dēfecta valor reventus sī rēs nūlla {@code illud} quadrat.
+   */
   public static @NotNull Gradus ut(@NotNull final Enum<@NotNull ?> illud, @NotNull final Gradus dēfecta) {
     try {
       return ut(illud);
@@ -42,6 +85,11 @@ public enum Gradus {
     }
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Gradus} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @implSpec {@code null} veniet sī rēs nūlla {@code illud} quadrat.
+   */
   @SuppressWarnings("ConstantConditions")
   public static @NotNull Gradus ut(@NotNull final Enum<@NotNull ?> illud) throws EnumConstantNotPresentException {
     final Gradus hoc = (Gradus) illud;
