e2751580584e3aa17a7036be3705dddf3a737e02
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2021-04-17 21:42:14 -0500

    Continue Javadoc additions

--- src/main/java/net/strūctorverba/ēnumerātiōnēs/Catēgoria.java
+++ src/main/java/net/strūctorverba/ēnumerātiōnēs/Catēgoria.java
@@ -3,21 +3,70 @@ package net.strūctorverba.ēnumerātiōnēs;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Ēnumerātiō haec extēnsiōnem classī {@link Verbum} dēsignat cui verbum omne respondet.
+ */
 @SuppressWarnings({ "NonAsciiCharacters", "SpellCheckingInspection" })
 public enum Catēgoria {
+  /**
+   * Valor hic classem {@link Nōmen} respondet.
+   */
   NŌMEN("nōmina"),
+
+  /**
+   * Valor hic classem {@link Adiectīvum} respondet.
+   */
   ADIECTĪVUM("adiectīva"),
+
+  /**
+   * Valor hic classem {@link Prōnōmen} respondet.
+   */
   PRŌNŌMEN("prōnōmina"),
+
+  /**
+   * Valor hic classem {@link Āctum} respondet.
+   */
   ĀCTUM("ācta"),
+
+  /**
+   * Valor hic classem {@link Adverbium} respondet.
+   */
   ADVERBIUM("adverbia"),
+
+  /**
+   * Valor hic classem {@link Coniūnctīvum} respondet.
+   */
   CONIŪNCTĪVUM("coniūnctīva"),
+
+  /**
+   * Valor hic classem {@link Praepositiō} respondet.
+   */
   PRAEPOSITIŌ("praepositiōnēs"),
+
+  /**
+   * Valor hic classem {@link PrōnōmenConiūnctīvum} respondet.
+   */
   PRŌNŌMEN_CONIŪNCTĪVUM("prōnōminaConiūnctīva"),
+
+  /**
+   * Valor hic classem {@link Numerus} respondet.
+   */
   NUMERUM(StringUtils.EMPTY),
+
+  /**
+   * Valor hic classem {@link VerbaPraepositiōne} respondet.
+   */
   VERBA_PRAEPOSITIŌNE(StringUtils.EMPTY);
 
+  /**
+   * Valor hic nōminātur scrīnium intrā quō scāpum verbī omnī cōnservātum est.
+   */
   public final @NotNull String scrīptiō;
 
+  /**
+   * Officium hoc cōnstrūctōrem reī classis huius perpetrat.
+   * @param scrpt valōrem {@link #scrīptiō} indicat.
+   */
   Catēgoria(@NotNull final String scrpt) {
     scrīptiō = scrpt;
   }
