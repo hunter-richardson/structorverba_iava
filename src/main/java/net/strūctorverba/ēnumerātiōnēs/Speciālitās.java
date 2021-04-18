3a3212b28136f11e605f61d8ead03467ba8707b5
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2021-04-17 23:06:52 -0500

    Continue Javadoc additions

--- src/main/java/net/strūctorverba/ēnumerātiōnēs/Speciālitās.java
+++ src/main/java/net/strūctorverba/ēnumerātiōnēs/Speciālitās.java
@@ -7,31 +7,70 @@ import org.jetbrains.annotations.*;
 
 import java.util.stream.Stream;
 
+/**
+ * Ēnumerātiō haec dēsignat sīve verbum est certum sīve incertum.
+ */
 @SuppressWarnings({ "NonAsciiCharacters", "SpellCheckingInspection", "unused" })
 public enum Speciālitās {
+  /**
+   * Valor hic ūtātur sī ūsūfructuāriī valōrem validum nōn dēnotat.
+   * Numquam scrīnium <a href="../src/main/resources">auxiliārēs</a> inscrībētur.
+   */
   NŪLLUM(StringUtils.EMPTY),
+
+  /**
+   * Valor hic verbum certum dēsignat.
+   */
   COMMŪNE("commūne"),
+
+  /**
+   * Valor hic verbum incertum dēsignat.
+   */
   PROPRIUM("proprium");
 
   @Getter(lazy = true)
   private static final @NotNull Nūntius.NūntiusErrōribus nūntius = Nūntius.NūntiusErrōribus.fac.get();
 
+  /**
+   * Valor hic repraesentātiōnem scrīpta reī huius dēsignat.
+   */
   public final @NotNull String scrīptiō;
 
+  /**
+   * Officium hoc cōnstrūctōrem reī classis huius perpetrat.
+   * @param scrpt valōrem {@link #scrīptiō} indicat.
+   */
   Speciālitās(@NotNull final String scrpt) {
     scrīptiō = scrpt;
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Speciālitās} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Speciālitās} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @implSpec {@code null} veniet sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Speciālitās dēfīniam(@NotNull final String scrīptiō) {
     return dēfīniam(scrīptiō, null);
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Speciālitās} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Speciālitās} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @param dēfecta valor reventus sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Speciālitās dēfīniam(@NotNull final String scrīptiō, @Nullable final Speciālitās dēfecta) {
     return Stream.of(values())
                  .filter(speciālitās -> speciālitās.scrīptiō.equals(scrīptiō))
                  .findFirst().orElse(dēfecta);
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Speciālitās} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @param dēfecta valor reventus sī rēs nūlla {@code illud} quadrat.
+   */
   public static @NotNull Speciālitās ut(@NotNull final Enum<@NotNull ?> illud, @NotNull final Speciālitās dēfecta) {
     try {
       return ut(illud);
@@ -41,6 +80,11 @@ public enum Speciālitās {
     }
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Speciālitās} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @implSpec {@code null} veniet sī rēs nūlla {@code illud} quadrat.
+   */
   @SuppressWarnings("ConstantConditions")
   public static @NotNull Speciālitās ut(@NotNull final Enum<@NotNull ?> illud) throws EnumConstantNotPresentException {
     final Speciālitās hoc = (Speciālitās) illud;
