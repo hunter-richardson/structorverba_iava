c8d0e9ec7a5451e64c5d5b7556189a2ec48efd73
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2021-04-17 23:02:01 -0500

    Continue Javadoc additions

--- src/main/java/net/strūctorverba/ēnumerātiōnēs/Persōna.java
+++ src/main/java/net/strūctorverba/ēnumerātiōnēs/Persōna.java
@@ -7,32 +7,75 @@ import org.jetbrains.annotations.*;
 
 import java.util.stream.Stream;
 
+/**
+ * Ēnumerātiō haec persōnam verbī omnis dēsignat.
+ */
 @SuppressWarnings({ "NonAsciiCharacters", "SpellCheckingInspection", "unused" })
 public enum Persōna {
-  NŪLLUM(StringUtils.EMPTY),
-  PRĪMUM("prīmum"),
-  SECUNDUM("secundum"),
-  TERTIUM("tertium");
+  /**
+   * Valor hic ūtātur sī ūsūfructuāriī valōrem validum nōn dēnotat.
+   * Numquam scrīnium <a href="../src/main/resources">auxiliārēs</a> inscrībētur.
+   */
+  NŪLLA(StringUtils.EMPTY),
+
+  /**
+   * Valor hic persōnam prīmam dēsignat.
+   */
+  PRĪMA("prīma"),
+
+  /**
+   * Valor hic persōnam secundam dēsignat.
+   */
+  SECUNDA("secunda"),
+
+  /**
+   * Valor hic persōnam tertiam dēsignat.
+   */
+  TERTIA("tertia");
 
   @Getter(lazy = true)
   private static final @NotNull Nūntius.NūntiusErrōribus nūntius = Nūntius.NūntiusErrōribus.fac.get();
 
+  /**
+   * Valor hic repraesentātiōnem scrīpta reī huius dēsignat.
+   */
   public final @NotNull String scrīptiō;
 
+  /**
+   * Officium hoc cōnstrūctōrem reī classis huius perpetrat.
+   * @param scrpt valōrem {@link #scrīptiō} indicat.
+   */
   Persōna(@NotNull final String scrpt) {
     scrīptiō = scrpt;
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Persōna} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Persōna} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @implSpec {@code null} veniet sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Persōna dēfīniam(@NotNull final String scrīptiō) {
     return dēfīniam(scrīptiō, null);
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Persōna} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Persōna} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @param dēfecta valor reventus sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Persōna dēfīniam(@NotNull final String scrīptiō, @Nullable final Persōna dēfecta) {
     return Stream.of(values())
                  .filter(persōna -> persōna.scrīptiō.equals(scrīptiō))
                  .findFirst().orElse(dēfecta);
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Persōna} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @param dēfecta valor reventus sī rēs nūlla {@code illud} quadrat.
+   */
   public static @NotNull Persōna ut(@NotNull final Enum<@NotNull ?> illud, @NotNull final Persōna dēfecta) {
     try {
       return ut(illud);
@@ -42,6 +85,11 @@ public enum Persōna {
     }
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Persōna} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @implSpec {@code null} veniet sī rēs nūlla {@code illud} quadrat.
+   */
   @SuppressWarnings("ConstantConditions")
   public static @NotNull Persōna ut(@NotNull final Enum<@NotNull ?> illud) throws EnumConstantNotPresentException {
     final Persōna hoc = (Persōna) illud;
