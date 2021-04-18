b2056cb2041ad1cf3dda2f7e2ef9c3754a2fe088
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2021-04-17 22:28:50 -0500

    Continue Javadoc additions

--- src/main/java/net/strūctorverba/ēnumerātiōnēs/Genus.java
+++ src/main/java/net/strūctorverba/ēnumerātiōnēs/Genus.java
@@ -7,32 +7,75 @@ import org.jetbrains.annotations.*;
 
 import java.util.stream.Stream;
 
+/**
+ * Ēnumerātiō haec generem verbī omnis dēsignat.
+ */
 @SuppressWarnings({ "NonAsciiCharacters", "SpellCheckingInspection", "unused" })
 public enum Genus {
+  /**
+   * Valor hic ūtātur sī ūsūfructuāriī valōrem validum nōn dēnotat.
+   * Numquam scrīnium <a href="../src/main/resources">auxiliārēs</a> inscrībētur.
+   */
   NŪLLUM(StringUtils.EMPTY),
+
+  /**
+   * Valor hic generem neutrum dēsignat.
+   */
   NEUTRUM("neutrum"),
+
+  /**
+   * Valor hic generem masculīnum dēsignat.
+   */
   MASCULĪNUM("masculīnum"),
+
+  /**
+   * Valor hic generem fēminīnum dēsignat.
+   */
   FĒMINĪNUM("fēminīnum");
 
   @Getter(lazy = true)
   private static final @NotNull Nūntius.NūntiusErrōribus nūntius = Nūntius.NūntiusErrōribus.fac.get();
 
+  /**
+   * Valor hic repraesentātiōnem scrīpta reī huius dēsignat.
+   */
   public final @NotNull String scrīptiō;
 
+  /**
+   * Officium hoc cōnstrūctōrem reī classis huius perpetrat.
+   * @param scrpt valōrem {@link #scrīptiō} indicat.
+   */
   Genus(@NotNull final String scrpt) {
     scrīptiō = scrpt;
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Genus} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Genus} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @implSpec {@code null} veniet sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Genus dēfīniam(@NotNull final String scrīptiō) {
     return dēfīniam(scrīptiō, null);
   }
 
+  /**
+   * Modus hic rem ēnumerātiō {@link Genus} ā parametrō dēsignātō advenit.
+   * @return rem ēnumerātiō {@link Genus} quod parametrum dēsignātum quadrat.
+   * @param scrīptiō valōrem {@link #scrīptiō} indicat.
+   * @param dēfecta valor reventus sī rēs nūlla parametrum quadrat.
+   */
   public static @Nullable Genus dēfīniam(@NotNull final String scrīptiō, @Nullable final Genus dēfecta) {
     return Stream.of(values())
                  .filter(genus -> genus.scrīptiō.equals(scrīptiō))
                  .findFirst().orElse(dēfecta);
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Genus} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @param dēfecta valor reventus sī rēs nūlla {@code illud} quadrat.
+   */
   public static @NotNull Genus ut(@NotNull final Enum<@NotNull ?> illud, @NotNull final Genus dēfecta) {
     try {
       return ut(illud);
@@ -42,6 +85,11 @@ public enum Genus {
     }
   }
 
+  /**
+   * @return {@code illud} sī valōrem ēnumerātiō {@link Genus} quadrat; {@code dēfecta} aliter.
+   * @param illud valor tentandus.
+   * @implSpec {@code null} veniet sī rēs nūlla {@code illud} quadrat.
+   */
   @SuppressWarnings("ConstantConditions")
   public static @NotNull Genus ut(@NotNull final Enum<@NotNull ?> illud) throws EnumConstantNotPresentException {
     final Genus hoc = (Genus) illud;
